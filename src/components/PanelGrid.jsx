5667dfdc9f2fa6a642c570617491374ef7419df4
lp-jordan <jordan@leaderpass.com>
2025-08-15 14:21:41 -0400

    Update PanelGrid.jsx

--- src/components/PanelGrid.jsx
+++ src/components/PanelGrid.jsx
@@ -1,41 +1,49 @@
 import PanelCard from "./PanelCard";
 
+import readImg from "../assets/panels/read.jpg";
+import buyImg from "../assets/panels/buy.jpg";
+import worldImg from "../assets/panels/world.jpg";
+import teamImg from "../assets/panels/team.jpg";
+import contactImg from "../assets/panels/contact.jpg";
+
 export default function PanelGrid() {
   return (
     <div className="grid grid-rows-3 gap-4 w-full h-full">
       <div className="grid h-full grid-cols-1 gap-4">
         <PanelCard
-          imageSrc="https://via.placeholder.com/600x300?text=Read"
+          className="bg-white h-full"
+          imageSrc={readImg}
           label="READ"
           to="/read"
         />
       </div>
       <div className="grid h-full grid-cols-2 gap-4">
         <PanelCard
-          imageSrc="https://via.placeholder.com/300x200?text=Buy"
+          className="bg-white h-full"
+          imageSrc={buyImg}
           label="BUY"
           to="/buy"
         />
         <PanelCard
-          imageSrc="https://via.placeholder.com/300x200?text=World"
+          className="bg-white h-full"
+          imageSrc={worldImg}
           label="WORLD"
           to="/world"
         />
       </div>
       <div className="grid h-full grid-cols-3 gap-4">
         <PanelCard
-          className="col-span-1"
-          imageSrc="https://via.placeholder.com/200x133?text=Team"
+          className="bg-white h-full col-span-1"
+          imageSrc={teamImg}
           label="TEAM"
           to="/team"
         />
         <PanelCard
-          className="col-span-2"
-          imageSrc="https://via.placeholder.com/400x267?text=Contact"
+          className="bg-white h-full col-span-2"
+          imageSrc={contactImg}
           label="CONTACT"
           to="/contact"
         />
       </div>
     </div>
   );
-}
