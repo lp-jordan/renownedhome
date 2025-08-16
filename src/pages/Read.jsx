0d55e0bbeefd0e5ef9fde05b7173bbdacf6d454f
lp-jordan <jordan@leaderpass.com>
2025-08-16 16:11:00 -0400

    Add horizontal IssueCarousel component

--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -1,15 +1,20 @@
 import PanelContent from "../components/PanelContent";
 import { motion } from "framer-motion";
+import IssueCarousel from "../components/IssueCarousel";
 
 export default function Read() {
   return (
     <PanelContent>
-      <motion.h1
-        layoutId="READ"
-        className="text-4xl font-bold uppercase"
-      >
-        READ
-      </motion.h1>
+      <div className="flex flex-col items-center justify-center w-full h-full space-y-8">
+        <motion.h1
+          layoutId="READ"
+          className="text-4xl font-bold uppercase"
+        >
+          READ
+        </motion.h1>
+        <IssueCarousel />
+      </div>
     </PanelContent>
   );
 }
+
