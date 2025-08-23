ea5d13c0680bb5ed8225259ae801ec4c398660c4
lp-jordan <jordan@leaderpass.com>
2025-08-23 13:49:13 -0400

    fix: import WordPress media hook

--- src/components/IssueInfoPanel.jsx
+++ src/components/IssueInfoPanel.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import useWordPressMedia from "../hooks/useWordPressMedia";
 import ImageWithFallback from "./ImageWithFallback";
 import { fetchMediaById } from "../api/wordpress";
 
