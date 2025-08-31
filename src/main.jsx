ec6398a768d9c0e84d6a48ed1c0172c6dc256682
lp-jordan <jordan@leaderpass.com>
2025-08-31 13:38:34 -0400

    feat: add global media store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { MediaProvider } from "./hooks/useMediaStore";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MediaProvider>
+        <App />
+      </MediaProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
