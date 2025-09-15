437f0aa4f7a2deb4124e7f7658d035e0565d9d08
lp-jordan <jordan@leaderpass.com>
2025-09-15 00:15:37 -0400

    Revert "Align panel labels with subpage headers"

--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -16,7 +16,7 @@ export default function Read() {
         <motion.h1
           layoutId={`panel-label-${panel.main.name}`}
           transition={{ duration: 0.4 }}
-          className={`inline-block ${heading.className} ${heading.size} text-center`}
+          className={`${heading.className} ${heading.size} mb-2 text-center`}
         >
           {heading.text}
         </motion.h1>
