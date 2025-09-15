b6321c1dd15f1427702d1388e3a2075e8cc04df9
lp-jordan <jordan@leaderpass.com>
2025-09-14 23:34:35 -0400

    Handle panel morph for nested routes

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -3,3 +3,8 @@ export const updatePreviousPathname = (path) => {
   previousPathname = path;
 };
 export const getPreviousPathname = () => previousPathname;
+
+export const getFirstPathSegment = (path = "/") => {
+  const parts = path.split("/").filter(Boolean);
+  return parts.length ? `/${parts[0]}` : "/";
+};
